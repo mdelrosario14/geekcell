2205d9cc812f0cb78ef1d0439a8281470dd4b10a
MardolfhDelRosario <mdelrosario14@gmail.com>
2019-06-02 19:37:30 -0500

    First Run done!

---
+++ src.main.java/com/gc/GeekCellApplication.java
@@ -0,0 +1,19 @@
+/**
+ * GeekCellApplication.java - GeekCellApplication main application.
+ * 2019 All rights reserved.
+ *
+ */
+package com.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekCellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekCellApplication.class, args);
+
+	}
+
+}
